7f988b756c4f52fe620cf7d6cb2d60a2b5aef91a
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-05-08 22:30:25 +0800

    Added responsive on all section

--- js/plugin.js
+++ js/plugin.js
@@ -1,40 +1,49 @@
-var toggleBtn = document.querySelector(".header__main-nav--toggle"),
-    navBar    = document.querySelector(".header__nav-bar"),
-    hasDropDown = document.querySelector(".header__main-nav--carret"),
-    menuTabs = document.querySelectorAll(".header__nav-bar ul li a"),
-    window_width = window.innerWidth;
 
-    console.log(hasDropDown);
+let window_width = window.innerWidth;
 
-toggleBtn.addEventListener("click", function(){
-    if(navBar.classList.contains("header__nav-bar--show")){
-        navBar.classList.remove("header__nav-bar--show");
-    }else{
-        navBar.classList.add("header__nav-bar--show");
-    }
-});
 
-hasDropDown.addEventListener("click", function(){
-    if(this.nextElementSibling.classList.contains("header__mega-menu--show")){
-        this.nextElementSibling.classList.remove("header__mega-menu--show");
-    }else{
-        this.nextElementSibling.classList.add("header__mega-menu--show")
-    }
+if(document.querySelector(".header__main-nav--toggle")){
+    let toggleBtn = document.querySelector(".header__main-nav--toggle"),
+        navBar    = document.querySelector(".header__nav-bar");
 
-    if(this.firstChild.classList.contains("fa-caret-down")){
-        this.firstChild.classList.remove("fa-caret-down");
-        this.firstChild.classList.add("fa-caret-up");
-    }else{
-        this.firstChild.classList.add("fa-caret-down");
-        this.firstChild.classList.remove("fa-caret-up");
-    }
+    toggleBtn.addEventListener("click", function(){
+        if(navBar.classList.contains("header__nav-bar--show")){
+            navBar.classList.remove("header__nav-bar--show");
+        }else{
+            navBar.classList.add("header__nav-bar--show");
+        }
+    });
+}
 
-});
+if(document.querySelector(".header__main-nav--carret")){
+    let hasDropDown = document.querySelector(".header__main-nav--carret");
+    hasDropDown.addEventListener("click", function(){
+        if(this.nextElementSibling.classList.contains("header__mega-menu--show")){
+            this.nextElementSibling.classList.remove("header__mega-menu--show");
+        }else{
+            this.nextElementSibling.classList.add("header__mega-menu--show")
+        }
+    
+        if(this.firstChild.classList.contains("fa-caret-down")){
+            this.firstChild.classList.remove("fa-caret-down");
+            this.firstChild.classList.add("fa-caret-up");
+        }else{
+            this.firstChild.classList.add("fa-caret-down");
+            this.firstChild.classList.remove("fa-caret-up");
+        }
+    
+    });
+}
+
 
 function removeMenuTabEvents(){
-    for (let index = 0; index < menuTabs.length; index++) {
-        menuTabs[index].removeAttribute("onmouseover");
+    if(document.querySelectorAll(".header__nav-bar ul li a")){
+        menuTabs = document.querySelectorAll(".header__nav-bar ul li a");
+        for (let index = 0; index < menuTabs.length; index++) {
+            menuTabs[index].removeAttribute("onmouseover");
+        }
     }
+
 }
 
 if(window_width <= 991){
@@ -46,4 +55,22 @@ window.onresize = function(event){
     if(window_width <= 991){
         removeMenuTabEvents();
     }
+}
+
+
+if(document.querySelector(".footer__middle--site-map")){
+    let footerTab = document.getElementsByClassName("footer__middle--site-map");
+    for (let index = 0; index < footerTab.length; index++) {
+        footerTab[index].addEventListener("click", function(){
+            if(this.classList.contains("footer__arrow")){
+                this.classList.remove("footer__arrow");
+                this.lastElementChild.classList.remove("footer__nav--show");
+            }else{
+                this.classList.add("footer__arrow");
+                this.lastElementChild.classList.add("footer__nav--show");
+            }
+
+        });
+    }
+
 }
