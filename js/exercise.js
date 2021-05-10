702804c19a18b55843fe07bfdafe8ceb8f0cd0b9
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-05-10 19:21:10 +0800

    Exercise

--- js/exercise.js
+++ js/exercise.js
@@ -18,3 +18,28 @@ console.log(document.getElementById("bottom2"));
 console.log(document.getElementById("bottom3"));
 console.log(document.getElementById("search"));
 
+// Get 5 elements using Classname
+let btm_news = document.getElementsByClassName("bottom4__news")
+console.log(btm_news);
+
+// querySelector
+let idList = document.querySelector("#bottom1"),
+    classList = document.querySelectorAll(".bottom4__news");
+console.log(idList);
+console.log(classList);
+
+console.log(document.querySelectorAll("div, p"));
+
+for (let index = 0; index < classList.length; index++) {
+    classList[index].style.background = "red";
+}
+
+let btn = document.createElement("button")
+btn.innerHTML = "Submit";
+btn.classList.add("btn-blue");
+idList.appendChild(btn);
+
+let footer = document.createElement("footer");
+footer.innerHTML = '<section class="wrapper"><h1>Hello World!</section>';
+document.querySelector(".footer").appendChild(footer);
+
