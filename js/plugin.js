cf76fc3ea2735568460421a7ee2157cedfab3eed
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-05-15 00:03:45 +0800

    Ecommerce Functionalities not done

--- js/plugin.js
+++ js/plugin.js
@@ -1,3 +1,7 @@
+let subtotalPrice = document.querySelector("#cart-section__subtotal"),
+totalPrice = document.querySelector("#cart-section__total"),
+total_items = document.querySelector(".header__top--cart-items");
+
 //#region Header and Footer Functionalities
 let window_width = window.innerWidth;
 
@@ -57,20 +61,6 @@ window.onresize = function(event){
     if(window_width <= 991){
         removeMenuTabEvents();
     }
-
-    // if(document.querySelector(".breadcrumbs__tab--active")){
-    //     let breadcrumbs__tab = document.querySelector(".breadcrumbs__tab--active");
-    //     const li_width = breadcrumbs__tab.clientWidth;
-    //     if(window_width <= 480){
-    //         breadcrumbs__tab.style.width = `${li_width - counter}px`;
-    //     }else{
-    //         counter = 1;
-    //         breadcrumbs__tab.style.width = `${li_width}px`;
-    //     }
-    //     counter++;
-    // console.log(li_width - counter);
-
-    // }
 }
 
 
@@ -95,64 +85,129 @@ if(document.querySelector(".footer__middle--site-map")){
 
 //#region Quantity Function
 
-var cartObj = [
-    {'product_id': 'c04297141','product_image': 'c04297141.png', 'product_title': 'Philips Hue 7W BR30 Connected Downlight Lamp', 'product_price': 499, 'product_quantity': 2},
-    {'product_id': 'macbook-pro','product_image': 'macbook-pro.png', 'product_title': 'Apple MacBook Pro', 'product_price': 499, 'product_quantity': 2}
-]
+var cartObj = [];
+//     {'product_id': 'c04297141','product_image': 'c04297141.png', 'product_title': 'Philips Hue 7W BR30 Connected Downlight Lamp', 'product_price': 499, 'product_quantity': 2}
+// ]
 
 // Put the object into storage
-localStorage.setItem('cartObj', JSON.stringify(cartObj));
+
+if(localStorage.getItem('cartObj')){
+    JSON.parse(localStorage.getItem('cartObj')).forEach(e => {
+        cartObj.push(e);
+    });   
+    totalItems();
+}
 
 
+document.addEventListener('click', function(e){
+    if(hasClass(e.target, 'bottom1__card__add-to-cart')){
+        
+        cartObj.push(
+            {'product_id': e.target.offsetParent.offsetParent.offsetParent.offsetParent.querySelector('.bottom1__card__image').getAttribute('src').replace('images/',''),
+            'product_image': e.target.offsetParent.offsetParent.offsetParent.offsetParent.querySelector('.bottom1__card__image').getAttribute('src').replace('images/',''), 
+            'product_title': e.target.offsetParent.offsetParent.offsetParent.offsetParent.querySelector('.bottom1__card__title').textContent, 
+            'product_price': e.target.offsetParent.offsetParent.offsetParent.offsetParent.querySelector('.bottom1__card__price').textContent.replace('$',''), 
+            'product_quantity': 1}
+        );
+        localStorage.setItem('cartObj', JSON.stringify(cartObj));
+        totalItems();
 
-JSON.parse(localStorage.getItem('cartObj')).forEach(element => {
-    let cartList = document.querySelector('.cart-section'),
-        div = document.createElement('div');
-
-        div.classList.add('cart-section__table-row');
-        div.setAttribute('id', element.product_id);
-        div.innerHTML = `<div class="cart-section__table-row--product">
-        <button class="cart-section__table-row--product--cancel-item-btn"><i class="fas fa-times"></i></button>
-        <img src="images/${element.product_image}">
-        ${element.product_title}
-    </div>
-    <div class="cart-section__table-row--unit-price"><span class="cart-section__table-row--unit-price--label">Unit Price</span>${element.product_price}</div>
-    <div class="cart-section__table-row--qty">
-        <button class="cart-section__table-row--qty--minus-btn" id="qty-minus-btn">-</button>
-        <div class="cart-section__table-row--qty--number">${element.product_quantity}</div>
-        <button class="cart-section__table-row--qty--plus-btn" id="qty-plus-btn">+</button>
-    </div>
-    <div class="cart-section__table-row--price"><span class="cart-section__table-row--price--label">Price</span>$998</div>`;
-
-        cartList.appendChild(div)
-
-    console.log(element.product_image);
+    }
 });
+
+
+if(localStorage.getItem('cartObj')){
+    JSON.parse(localStorage.getItem('cartObj')).forEach(element => {
+        if(document.querySelector('.cart-section__product-list')){
+            let cartList = document.querySelector('.cart-section__product-list'),
+            div = document.createElement('div');
+    
+            div.classList.add('cart-section__table-row');
+            div.setAttribute('id', element.product_id);
+            div.innerHTML = 
+            `<div class="cart-section__table-row--product">
+                <button class="cart-section__table-row--product--cancel-item-btn"><i class="fas fa-times"></i></button>
+                <img src="images/${element.product_image}">${element.product_title}
+            </div>
+            <div class="cart-section__table-row--unit-price">
+                <span class="cart-section__table-row--unit-price--label">Unit Price</span>${element.product_price}
+            </div>
+            <div class="cart-section__table-row--qty">
+                <button class="cart-section__table-row--qty--minus-btn" id="qty-minus-btn">-</button>
+                <div class="cart-section__table-row--qty--number">${element.product_quantity}</div>
+                <button class="cart-section__table-row--qty--plus-btn" id="qty-plus-btn">+</button>
+            </div>
+            <div class="cart-section__table-row--price">
+                <span class="cart-section__table-row--price--label">Price</span>
+                <span class="cart-section__table-row--price--label-price">$${element.product_quantity * element.product_price}</span>
+            </div>`;
+    
+            cartList.appendChild(div);
+    
+            subtotalPrice.textContent = `$ ${subtotal().subtotal}`;
+            totalPrice.textContent = `$ ${subtotal().totalPrice}`;
+        }
+    });
+}
 
 // Retrieve the object from storage
 
-    const remainQuantity = 20;
-    document.addEventListener("click", function(e){
-        let prod_quantity = e.target.previousElementSibling;
-        if(e.target.id.split(' ').indexOf("qty-plus-btn") >- 1){
-            if(parseInt(e.target.previousElementSibling.textContent, 10) !== remainQuantity )
-                e.target.previousElementSibling.textContent = parseInt(e.target.previousElementSibling.textContent, 10) + 1;
+const remainQuantity = 20;
+document.addEventListener("click", function(e){
+    if(e.target.id.split(' ').indexOf("qty-plus-btn") >- 1){
+        let prod_quantity = e.target.previousElementSibling,
+            prod_price = e.target.parentElement.nextElementSibling.lastElementChild;
 
-            console.log(e.target);
-            if(document.querySelectorAll(".cart-section__table-row--price")){
-                e.target.parentElement.nextElementSibling.textContent = `$${parseInt(e.target.previousElementSibling.textContent, 10) * 499}`
-            }
+        if(parseInt(prod_quantity.textContent, 10) !== remainQuantity )
+            prod_quantity.textContent = parseInt(prod_quantity.textContent, 10) + 1;
+        if(document.querySelectorAll(".cart-section__table-row--price--label-price"))
+            prod_price.textContent = `$${parseInt(prod_quantity.textContent, 10) * 499}`
 
-        }else if(e.target.id.split(' ').indexOf("qty-minus-btn") >- 1){
-            if(parseInt(e.target.nextElementSibling.textContent, 10) > 1)
-                e.target.nextElementSibling.textContent = parseInt(e.target.nextElementSibling.textContent, 10) - 1
-        }else if(hasClass(e.target, "cart-section__table-row--product--cancel-item-btn"))
-            e.target.parentElement.parentElement.remove();
+        subtotalPrice.textContent = `$ ${subtotal().subtotal}`;
+        totalPrice.textContent = `$ ${subtotal().totalPrice}`;
 
+    }else if(e.target.id.split(' ').indexOf("qty-minus-btn") >- 1){
+        let prod_quantity = e.target.nextElementSibling,
+            prod_price = e.target.parentElement.nextElementSibling.lastElementChild;
+
+        if(parseInt(prod_quantity.textContent, 10) > 1){
+            prod_quantity.textContent = parseInt(prod_quantity.textContent, 10) - 1
+            if(document.querySelectorAll(".cart-section__table-row--price--label-price"))
+                prod_price.textContent = `$${parseInt(prod_quantity.textContent, 10) * 499}`
+        }
+        subtotalPrice.textContent = `$ ${subtotal().subtotal}`;
+        totalPrice.textContent = `$ ${subtotal().totalPrice}`;
+    }else if(hasClass(e.target, "cart-section__table-row--product--cancel-item-btn")){
+        e.target.parentElement.parentElement.remove();
+        subtotalPrice.textContent = `$ ${subtotal().subtotal}`;
+        totalPrice.textContent = `$ ${subtotal().totalPrice}`;
+        console.log(subtotal());
+    }
+
+});
+
+function subtotal(){
+    let subtotalPrice = document.querySelectorAll('.cart-section__table-row--price--label-price'), 
+        shipping = parseFloat(document.querySelector("#cart-section__shipping").textContent.replace('$', '')),
+        obj = {},
+        total = 0.00;
+    subtotalPrice.forEach(element =>{
+         total += parseFloat(element.textContent.replace('$',''));
     });
+
+    obj.subtotal = total.toFixed(2);
+    obj.totalPrice = (parseFloat(total) + shipping).toFixed(2);
+
+    return obj;
+}
 
 function hasClass(elem, className) {
     return elem.className.split(' ').indexOf(className) > -1;
+}
+
+function totalItems(){
+    if(total_items && JSON.parse(localStorage.getItem('cartObj')))
+        total_items.textContent = `${JSON.parse(localStorage.getItem('cartObj')).length} Items`; 
 }
 //#endregion
 
